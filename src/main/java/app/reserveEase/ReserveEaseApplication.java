0f09fe163531f2e95f8106f0b0c2809fc4e81d44
Golluméo <p.mauriello54@gmail.com>
2024-08-12 23:03:07 +0200

    chore: suppress IntelliJ unused class warning

--- src/main/java/app/reserveEase/ReserveEaseApplication.java
+++ src/main/java/app/reserveEase/ReserveEaseApplication.java
@@ -2,8 +2,10 @@ package app.reserveEase;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "app.reserveEase.web.controllers")
 public class ReserveEaseApplication {
 
 	public static void main(String[] args) {
