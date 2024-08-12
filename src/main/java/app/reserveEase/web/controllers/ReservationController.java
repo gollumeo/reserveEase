36168b742945d8e40cc469168eec330f877e8187
Golluméo <p.mauriello54@gmail.com>
2024-08-12 22:50:23 +0200

    chore: clean archi

--- src/main/java/app/reserveEase/web/controllers/ReservationController.java
+++ src/main/java/app/reserveEase/web/controllers/ReservationController.java
@@ -5,7 +5,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
-import java.util.Map;
 
 @RestController
 @RequestMapping("/reservations")
